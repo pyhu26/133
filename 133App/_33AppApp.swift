988d053d8eb09234a9aee42c4868aa7da4e3dabd
yoon.lee <pyhu26@gmail.com>
2025-11-01 10:21:21 +0900

    Initial Commit

---
+++ 133App/_33AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _33AppApp.swift
+//  133App
+//
+//  Created by yoonsmac on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct _33AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
